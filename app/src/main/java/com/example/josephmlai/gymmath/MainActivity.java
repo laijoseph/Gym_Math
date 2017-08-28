4975aaf0c34d09d143392e41c6f4b2c7c82da52a
Joseph Lai <joseph.m.lai@gmail.com>
2017-08-28 00:29:23 -0700

    Initial Setup is done.

--- app/src/main/java/com/example/josephmlai/gymmath/MainActivity.java
+++ app/src/main/java/com/example/josephmlai/gymmath/MainActivity.java
@@ -44,15 +44,97 @@ public class MainActivity extends AppCompatActivity {
 
     }else{
             out.setTextColor(Color.BLACK);
-            int difference = ((upperNum - lowerNum)-45)/2;
-            findPlates();
-            ans = Integer.toString(difference);
+            double difference = ((upperNum - lowerNum)-45)/2;
+            double[] initial = initPlates((lowerNum-45)/2);
+            double[] finl = plateDiff(initial, difference);
+            //ans = Integer.toString(difference);
         }
         out.setText(ans);
     }
 
-    private String findPlates(){//returns string of plates
+    private double[] initPlates (double x) {//returns string of plates
 
-        return null;
+        double[] plateCountArr = new double[6];//45, 35, 25, 10, 5, 2.5
+
+
+        if (z%5 != 0){z -= 2.5; plateCountArr[5]++;}
+        z = z/5;//multiple of 5
+
+        plateCountArr[0] += z/18;//counts number of 45's needed.
+
+        //plates at 10 or above are stackable/desirable
+        switch (z%18) {
+            ;//looks up array for largest plate
+            case 17:
+                plateCountArr[1]++;
+                plateCountArr[2]+=2;
+                break;
+            case 16:
+                plateCountArr[0]++;
+                plateCountArr[1]++;
+                break;
+            case 15:
+                plateCountArr[2]+=3;
+                break;
+            case 14:
+                plateCountArr[0]++;
+                plateCountArr[2]++;
+                break;
+            case 13:
+                plateCountArr[0]++;
+                plateCountArr[3]+=2;
+                break;
+            case 12:
+                plateCountArr[1]++;
+                plateCountArr[2]++;
+                break;
+            case 11:
+                plateCountArr[0]++;
+                plateCountArr[3]++;
+                break;
+            case 10:
+                plateCountArr[2]+=2;
+                break;
+            case 9:
+                plateCountArr[0]++;
+                break;
+            case 8:
+                plateCountArr[1]++;
+                plateCountArr[4]++;
+                break;
+            case 7:
+                plateCountArr[1]++;
+                break;
+            case 6:
+                plateCountArr[2]++;
+                plateCountArr[4]++;
+                break;
+            case 5:
+                plateCountArr[2]++;
+                break;
+            case 4:
+                plateCountArr[3]+=2;
+                break;
+            case 3:
+                plateCountArr[3]++;
+                plateCountArr[4]++;
+                break;
+            case 2:
+                plateCountArr[3]++;
+                break;
+            case 1:
+                plateCountArr[4]++;
+                break;
+            case 0:
+                plateCountArr[]++;
+                break;
+        }
+        return plateCountArr;
+    }
+    double[] finl (double[] in, double diff){
+        double[] plateCountArr = in;
+
+
+        return plateCountArr;
     }
 }
